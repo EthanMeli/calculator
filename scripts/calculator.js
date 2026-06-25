e48aab35e46c2abcef302b984847e9e83b7a9d74
EthanMeli <etme6835@colorado.edu>
2026-06-25 18:51:42 +0200

    Display number selections with operator

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,3 +1,37 @@
+const numbers = Array.from(document.body.querySelectorAll(".number"));
+const operators = Array.from(document.body.querySelectorAll(".operator"));
+const equals = document.body.querySelector(".equals");
+
+const equation = document.body.querySelector(".equation");
+const result = document.body.querySelector(".result");
+
+let numA = 0;
+let numB = null;
+let op = null;
+
+for (const number of numbers) {
+  number.addEventListener("click", () => {
+    if (op === null) {
+      numA = Number(number.textContent);
+      result.textContent = `${numA}`;
+    } else {
+      numB = Number(number.textContent);
+      result.textContent = `${numA} ${op} ${numB}`
+    }
+  });
+}
+
+for (const operator of operators) {
+  if (operator.id) {
+      operator.addEventListener("click", () => {
+      op = operator.textContent;
+      result.textContent = `${numA} ${op}`;
+      console.log(op);
+    })
+  }
+}
+
+/* OPERATOR FUNCTIONS */
 function add(a, b) {
   return a + b;
 }
@@ -16,13 +50,13 @@ function divide(a, b) {
 
 function operate(a, b, op) {
   switch (op) {
-    case '+':
+    case '&plus;':
       return add(a, b);
-    case '-':
+    case '&minus;':
       return subtract(a, b);
-    case '*':
+    case '&times;':
       return multiply(a, b);
-    case '/':
+    case '&divide;':
       if (b === 0) return "DIV BY 0 ERR";
       else return divide(a, b);
     default:
