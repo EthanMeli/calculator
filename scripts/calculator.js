9bd971895311a2bc571e34d744340a3127c9d4b4
EthanMeli <etme6835@colorado.edu>
2026-06-25 17:27:58 +0200

    Create operate function and respective helper functions

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,1 +1,31 @@
-console.log("Hello World");
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(a, b, op) {
+  switch (op) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      if (b === 0) return "DIV BY 0 ERR";
+      else return divide(a, b);
+    default:
+      console.log(`Unknown operation: ${op}.`);
+  }
+}
